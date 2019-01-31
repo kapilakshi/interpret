07d89a1b7cc47f3414294bab4f9eb184d4f6ee0c
Kapilakshi <Kapilakshi@incedoinc.com>
2019-01-31 14:42:10 +0530

    ui

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  baseUrl : 'http://10.11.198.208:5001/tripwire/query/?query=',
+  path: '/interpret/',
+  feedbackUrl: 'http://10.11.198.208:5001/tripwire/feedback/',
+  fileUrl:'/interpret/assets/htmls/'
+};
