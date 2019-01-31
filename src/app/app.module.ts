dfbb9468d00a2a377a495d22da419a5dfe4b0940
Kapilakshi <Kapilakshi@incedoinc.com>
2019-01-31 12:37:17 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { DataViewModule } from 'primeng/dataview';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
+import {APP_BASE_HREF} from '@angular/common';
+import { environment } from '../environments/environment';
+import {ButtonModule} from 'primeng/button';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {DialogModule} from 'primeng/dialog';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ToastModule} from 'primeng/toast';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    DataViewModule,
+    ButtonModule,
+    RadioButtonModule,
+    DialogModule,
+    ToastModule
+    
+   
+    
+  ],
+  providers: [{provide: APP_BASE_HREF, useValue: environment.path}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit, Renderer2,ElementRef ,ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import {MessageService} from 'primeng/api';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css'],
+  providers: [MessageService]
+})
+export class HomepageComponent implements OnInit {
+  
+  public name:any;
+  public titleof:any;
+  public contentof:any;
+  public result:any;
+  public feedback:any;
+  public url:string;
+  public feedbackUrl :any;
+  public colorofbutton:string;
+  baseUrl = environment.baseUrl;
+  path = environment.path;
+  fUrl = environment.feedbackUrl;
+  fileUrl = environment.fileUrl;
+  display: boolean = false;
+ 
+ 
+  constructor(private http : HttpClient,private messageService: MessageService) {
+    
+   }
+  updateSearch(e:any) {
+    this.result = null;
+    this.url = this.baseUrl ;
+    this.feedbackUrl= this.fUrl;
+   this.name = e.target.value;
+   this.http.get(this.url + this.name)
+  .subscribe(res => {this.result  = res; });
+  }
+ checkedData(i,title,content)
+ {
+  
+   this.titleof=title;
+   this.contentof=content;
+  
+  
+ }
+ displayData(title,content,i,id)
+  {
+    if(title==this.titleof)
+    {
+      
+      this.http.post(this.feedbackUrl,{"_id": id,
+      "doc_type": "_doc","query_string": this.name}) // need to send doc_type dynamically
+      .subscribe(res => {this.feedback  = res['result']; 
+     
+      if(this.feedback == true)
+      {  
+          this.messageService.add({severity:'success', summary: 'Success Message', detail:'Feedback submitted', life:2000});
+      
+          
+      }
+      else{
+       
+        this.messageService.add({severity:'info', summary: 'Unsuccess Message', detail:'Feedback not submitted',life:2000});
+    
+        
+      }
+    });
+      
+ 
+    }
+    else
+    {
+      this.messageService.add({severity:'warn', summary: 'Information', detail:'Please select the radio button',life:2000});
+     
+    }
+ 
+  
+ }
+ 
+  
+ngOnInit() {
+  
+  }
+  
+
+}
